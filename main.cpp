e167a01fc65eda5210965c34ac2f844dcc7258ef
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2023-06-21 15:42:08 +0900

    ウィンドウを出す

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <Windows.h>
+#include "WinApp.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// 空のインスタンスを生成
+	WinApp* winApp = nullptr;
+
+	// ウィンドウズアプリケーションクラスのインスタンスを取得
+	winApp = WinApp::GetInstance();
+	// ゲームウィンドウを生成する
+	winApp->CreateGameWindow();
+
+	// メインループ
+	while (true) {
+
+		// ゲームが終了するメッセージを受けとったら
+		if (winApp->ProcessMessage()) {
+			// ゲーム終了
+			break;
+		}
+
+	}
+
+	// 終了したら生成したオブジェクトの解放を行う
+	winApp->TerminateGameWindow();
 
 	// 終了
 	return 0;
